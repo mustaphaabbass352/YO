4fa100d92edd1c3602c00c6bd335b5bea738036d
mustaphaabbass352 <79026489+mustaphaabbass352@users.noreply.github.com>
2026-05-01 20:51:38 +0000

    Fix next.config.js syntax

--- yo-app/next.config.js
+++ yo-app/next.config.js
@@ -1,9 +1,3 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    unoptimized: true,
-  },
-};
-
-export default nextConfig;
+/** @type {import('next').NextConfig} */ 
+const nextConfig = { images: { unoptimized: true } }; 
+module.exports = nextConfig; 
